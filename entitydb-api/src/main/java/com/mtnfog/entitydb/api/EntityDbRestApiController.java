b0919a3a2ecefeac87d8965c36531ead16acd5d0
jzonthemtn <jeffrey@mtnfog.com>
2016-09-26 17:08:29 -0400

    Removing commented code. Adding javadocs. Cleaning up exceptions

--- entitydb-api/src/main/java/com/mtnfog/entitydb/api/EntityDbRestApiController.java
+++ entitydb-api/src/main/java/com/mtnfog/entitydb/api/EntityDbRestApiController.java
@@ -37,13 +37,12 @@ import com.mtnfog.entitydb.model.entitystore.QueryResult;
 import com.mtnfog.entitydb.model.exceptions.MalformedAclException;
 import com.mtnfog.entitydb.model.exceptions.NonexistantEntityException;
 import com.mtnfog.entitydb.model.exceptions.api.BadRequestException;
+import com.mtnfog.entitydb.model.exceptions.api.InternalServerErrorException;
 import com.mtnfog.entitydb.model.exceptions.api.NotFoundException;
 import com.mtnfog.entitydb.model.exceptions.api.UnableToQueueEntitiesException;
 import com.mtnfog.entitydb.model.exceptions.api.UnauthorizedException;
 
 import java.util.Collection;
-
-import javax.servlet.http.HttpServletRequest;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -66,12 +65,23 @@ public class EntityDbRestApiController {
 	@Autowired
 	private QueuePublisher queuePublisher;
 					
+	/**
+	 * Queues an entities for ingest.
+	 * @param entities A collection of {@link Entities}.
+	 * @param acl An optional ACL for all entities. If not specified all entities
+	 * are visible to all users. 
+	 * @param authorization An optional Authorization header.
+	 * @throws MalformedAclException Thrown if the ACL is invalid.
+	 * @throws Exception Thrown if the entities cannot be queued for ingestion.
+	 * Check the server log for more information.
+	 */
 	@RequestMapping(value = "/api/entity", method = {RequestMethod.PUT, RequestMethod.POST})
 	@ResponseStatus(HttpStatus.OK)
 	public void store(
 			@RequestBody Collection<Entity> entities, 
 			@RequestParam(value="acl", required=false, defaultValue=Acl.WORLD) String acl,
-			@RequestHeader(value="Authorization", required=false) String authorization) {
+			@RequestHeader(value="Authorization", required=false) String authorization)
+			throws MalformedAclException, Exception {
 					
 		try {
 						
@@ -91,12 +101,23 @@ public class EntityDbRestApiController {
 			
 	}
 	
-	@RequestMapping(value = "/api/entity/{entityId}/acl", method = {RequestMethod.PUT, RequestMethod.POST})
+	/**
+	 * Modifies an entity's ACL.
+	 * @param entityId The ID of the entity.
+	 * @param acl The updated ACL for the entity.
+	 * @param authorization An optional Authorization header.
+	 * @throws NonexistantEntityException Thrown if the entity ID is invalid.
+	 * @throws MalformedAclException Thrown if the ACL is invalid.
+	 * @throws InternalServerErrorException Thrown if the entity's ACL cannot
+	 * be updated for other reasons. Check the server log for more information on the cause.
+	 */
+	@RequestMapping(value = "/api/entity/{entityId}/acl", method = {RequestMethod.PUT, RequestMethod.POST})	
 	@ResponseStatus(HttpStatus.OK)
 	public void store(
 			@PathVariable String entityId,
 			@RequestParam(value="acl", required=true) String acl,
-			@RequestHeader(value="Authorization", required=false) String authorization) {
+			@RequestHeader(value="Authorization", required=false) String authorization)
+			throws NonexistantEntityException, MalformedAclException, InternalServerErrorException {
 					
 		try {
 			
@@ -112,7 +133,7 @@ public class EntityDbRestApiController {
 			
 		} catch (Exception ex) {
 			
-			throw new UnableToQueueEntitiesException("Unable to queue entities.", ex);
+			throw new InternalServerErrorException("Unable to update entity's ACL.", ex);
 			
 		}
 		
@@ -120,42 +141,30 @@ public class EntityDbRestApiController {
 			
 	}
 	
+	/**
+	 * Executes an EQL query against the indexed entities.
+	 * @param query The EQL query.
+	 * @param continuous An optional parameter enables this query as a
+	 * continuous query. Set to 1 to enable and 0 to disable (default).
+	 * @param days The number of days to run this query continuously.
+	 * If not provided the default value is 90 days.
+	 * @param authorization An optional Authorization header.
+	 * @return A {@link QueryResult} containing the entities.
+	 * @throws UnauthorizedException Thrown if the authentication is invalid.
+	 * @throws BadRequestException Thrown if the EQL query is malformed.
+	 */
 	@RequestMapping(value = "/api/eql", method = RequestMethod.GET)
 	public @ResponseBody QueryResult eql(
-			HttpServletRequest httpServletRequest,
 			@RequestParam(value = "query") String query,
 			@RequestParam(value = "continuous", required = false, defaultValue = "0") int continuous,
 			@RequestParam(value = "days", required = false, defaultValue = "90") int days,
 			@RequestHeader(value = "Authorization", required = false) String authorization)			
-		throws UnauthorizedException, BadRequestException, MalformedAclException {			
+		throws UnauthorizedException, BadRequestException {			
 					
 		LOGGER.trace("Received EQL query: {}", query);
 				
 		return entityQueryService.eql(query, authorization, continuous, days);
 		
 	}
-	
-	/*@RequestMapping(value = "/api/query", method = RequestMethod.GET)
-	public @ResponseBody QueryResult query(
-			HttpServletRequest httpServletRequest,
-			@RequestParam(value = "entity", required = false) String entityText,
-			@RequestParam(value = "minConfidence", required = false, defaultValue = "0") int minConfidence,
-			@RequestParam(value = "maxConfidence", required = false, defaultValue = "100") int maxConfidence,
-			@RequestParam(value = "context", required = false) String context,
-			@RequestParam(value = "documentId", required = false) String documentId,
-			@RequestParam(value = "enrichment", required = false) String enrichment,
-			@RequestParam(value = "type", required = false) String type,
-			@RequestParam(value = "language", required = false) String language,
-			@RequestParam(value = "uri", required = false) String uri,
-			@RequestParam(value = "offset", required = false, defaultValue = "0") int offset,
-			@RequestParam(value = "limit", required = false, defaultValue = "10") int limit,
-			@RequestHeader(value = "Authorization", required = false) String authorization)
-		throws UnauthorizedException, BadRequestException, QueryGenerationException, EntityStoreException, MalformedAclException, InvalidQueryException {
-						
-		return entityQueryService.query(entityText, minConfidence,
-				maxConfidence, context, documentId, enrichment, type, 
-				language, uri, offset, limit, authorization);
-		
-	}*/
 
 }
