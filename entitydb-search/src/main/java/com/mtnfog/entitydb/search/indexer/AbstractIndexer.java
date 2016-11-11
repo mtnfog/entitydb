6b559fc1285b4df29bb0c20629353d5bc16a02d9
jzonthemtn <jeffrey@mtnfog.com>
2016-11-11 06:59:39 -0500

    Removing data-store dependencies.

--- entitydb-search/src/main/java/com/mtnfog/entitydb/search/indexer/AbstractIndexer.java
+++ entitydb-search/src/main/java/com/mtnfog/entitydb/search/indexer/AbstractIndexer.java
@@ -23,7 +23,7 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
