bc27c677f0004a03ae5db2836eee9d913d2476b1
jzonthemtn <jeffrey@mtnfog.com>
2017-01-14 08:41:53 -0500

    #18 Getting version from the pom.xml when building docker image.

--- scripts/packaging/docker/build-image.sh
+++ scripts/packaging/docker/build-image.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-if [ "$#" -ne 1 ]; then
-	echo "Usage: ./build-image [version]"
-	exit 1
-fi
-
-VERSION=$1
+echo "Getting the version from the pom.xml."
+VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version -f ../../../pom.xml | egrep -v '^\[|Downloading:' | tr -d ' \n'`
 
 docker build -t mtnfog/entitydb:$VERSION .
